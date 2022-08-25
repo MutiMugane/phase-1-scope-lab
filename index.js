30dfa09c5b743e232003b2307e841872400db86a
+ <+>
2022-08-25 21:19:05 +0300

    Complete Assignment

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // Write your solution in this file!
+
+var customerName= `bob`;
+const leastFavoriteCustomer="job";
+let bestCustomer;   
+
+
+function upperCaseCustomerName(){    
+      customerName.toUpperCase(); 
+      console.log(customerName.toUpperCase());
+} 
+upperCaseCustomerName()
+
+function setBestCustomer(){
+    var bestCustomer="not bob";
+    return bestCustomer;
+};
+
+
+function overwriteBestCustomer(){
+    const bestCustomer="maybe bob";
+    return bestCustomer;
+};
+
+function changeLeastFavoriteCustomer(leastFavoriteCustomer ="test"){
+   var  leastFavoriteCustomer= "jane";
+    
+};
+
